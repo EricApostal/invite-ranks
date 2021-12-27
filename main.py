86085b09f1cb607e6db56b05e1035a4ef5aaf1a5
sirtzn <ehapostal04@gmail.com>
2021-12-26 20:01:17 -0500

    Made a better startup function, fixed scanning, removed scanning cooldown.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import nextcord
-from nextcord.ext import commands
 from time import sleep as wait
 import asyncio
 import json 
+import subprocess
+import sys
+
+try:
+    print("[STARTUP] Attempting to load package 'nextcord'")
+    import nextcord
+    from nextcord.ext import commands
+    print('[STARTUP] nextcord succesfully imported')
+except:
+    print('[STARTUP] nextcord not found, attempting install')
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'nextcord'])
+    import nextcord
+    print('Succefully installed and imported nextcord!')
+
+
 
 intents = nextcord.Intents.default()
 intents.members = True
@@ -43,45 +56,57 @@ async def get_value_of_dict(dict):
         tmp.append(int(value))
     return tmp
 
+
+
+
+async def scanServer(message):
+    while True:
+        print('[Log] Checking for any new status updates')
+        invites = 0
+        allmembers = message.guild.members
+        print('[Log] Scanning all members, one moment...')
+        for member in message.guild.members: # iterate through all members using var 'member'
+            invites = 0
+            print(member)
+            for i in await message.guild.invites(): # iterate through every invite link from every member
+                if i.inviter == member: # if the inviter of an invite LINK was that member...
+                    invites += i.uses # ... iterate invite variable + the number of link uses
+                
+                
+            if invites > 0:
+                print(f'[Log] {member} has {invites} invites')
+                for key, value in reversed(ranks.items()):
+                    if invites >= key:
+                        for role in member.roles:
+                            if role.id in await get_value_of_dict(ranks) and await get_key(role.id) < key:
+                                role = message.guild.get_role(int(role.id))
+                                print(f'[Log] Removing Role "{role}"')
+                                await member.remove_roles(role)
+                                    
+                            #print(f'{member} has role {roles.id}')
+                            
+                            
+                        role = message.guild.get_role(int(value)) # new instance of role, same variable because it made the most sense
+                        if not role in member.roles:
+                            print(f'[Log] {member} has at least {key} invites, giving them the role @{value}')
+                            await member.add_roles(role)
+                            
+                        await scanServer(message)
+
+
+
 @client.event
 async def on_message(message):
     if message.content.startswith('!syncinvites'):
-        while True:
-            print('[Log] Checking for any new status updates')
-            invites = 0
-            allmembers = message.guild.members
-            print('[Log] Scanning all members, one moment...')
-            for member in message.guild.members: # iterate through all members using var 'member'
-                for i in await message.guild.invites(): # iterate through every invite link from every member
-                    if i.inviter == member: # if the inviter of an invite LINK was that member...
-                        invites += i.uses # ... iterate invite variable + the number of link uses
-                
-                
-                if invites > 0:
-                    print(f'[Log] {member} has {invites} invites')
-                    totalinvites = invites
-                    for key, value in reversed(ranks.items()):
-                        if totalinvites >= key:
-                            for role in member.roles:
-                                if role.id in await get_value_of_dict(ranks) and await get_key(role.id) < key:
-                                    role = message.guild.get_role(int(role.id))
-                                    print(f'[Log] Removing Role "{role}"')
-                                    await member.remove_roles(role)
-                                #print(f'{member} has role {roles.id}')
+        await scanServer(message)
+            
                             
-                            
-                            role = message.guild.get_role(int(value)) # new instance of role, same variable because it made the most sense
-                            if not role in member.roles:
-                                print(f'[Log] {member} has at least {key} invites, giving them the role @{value}')
-                                await member.add_roles(role)
 
-                            return
-
-            asyncio.sleep(60)
+            
 
 @client.event    
 async def on_ready():
-    print('[Log] Bot has authenticated and started successfully')
+    print('[Log] Bot has authenticated and started successfully, awaiting command "!syncinvites" (type command in Discord server to start the process)')
                 
 
 token = config['token']
