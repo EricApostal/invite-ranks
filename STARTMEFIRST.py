3cce7000b092fe0743b7603d8e1c35f7e6c42edd
SirTZN <60072374+SirTZN@users.noreply.github.com>
2021-12-26 20:05:39 -0500

    Delete STARTMEFIRST.py

--- STARTMEFIRST.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import sys
-# this just installs the requirements, there's likely a better way to do this
-# but it turned out to be the most effective one I could find.
-subprocess.check_call([sys.executable, "-m", "pip", "install", 'nextcord'])
